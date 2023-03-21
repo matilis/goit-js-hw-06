865aba7adcefe03235098dfabffb99111d5df762
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-03-21 22:44:21 +0100

    Update Task 04 - finished

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,13 +3,13 @@ let counterValue = 0;
 const btnPlus = document
   .querySelector('[data-action="increment"]')
   .addEventListener("click", () => {
-    counterValue = counterValue + 1;
+    counterValue += 1;
     document.getElementById("value").innerHTML = counterValue;
   });
 
 const btnMinus = document
   .querySelector('[data-action="decrement"]')
   .addEventListener("click", () => {
-    counterValue = counterValue - 1;
+    counterValue -= 1;
     document.getElementById("value").innerHTML = counterValue;
   });
