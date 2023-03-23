a4232e423de7430916a2f9dc3f0e10bd45c40f97
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-03-23 19:35:38 +0100

    Task 9 finished

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.body;
+const colorEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+btnEl.addEventListener("click", () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+});
