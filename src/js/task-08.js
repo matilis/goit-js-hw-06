759fb88315d68c6d3c689401db2ac12d04421e7a
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-03-23 17:50:43 +0100

    Task 8 finished

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    console.log("ALERT! Please fill in all the fields!");
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+});
